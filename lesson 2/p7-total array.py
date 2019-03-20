414935fd09bba1cd64205498993e47726296abb3
Helder Pereira <helder.pereira@xing.com>
2019-03-20 18:29:01 +0100

    homework

--- lesson 2/p7-total array.py
+++ lesson 2/p7-total array.py
@@ -1,4 +1,4 @@
-ages = [40, 25, 34, 38]
+ages = [40, 25, 34, 48, 18]
 
 counter = 0
 total = 0
@@ -17,3 +17,20 @@ print(average)
 # Homework
 # find out the highest value in the array and print it
 # find out the lowest value in the array and print it
+
+highest = ages[0]
+lowest = ages[0]
+
+counter = 0
+while counter < len(ages):
+
+    if ages[counter] > highest:
+        highest = ages[counter]
+
+    if ages[counter] < lowest:
+        lowest = ages[counter]
+
+    counter += 1
+
+print("H: " + str(highest))
+print("L: " + str(lowest))
