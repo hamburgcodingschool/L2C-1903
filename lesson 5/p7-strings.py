93d7f965d84888bd039f6f4311ff3b4b15729067
Helder Pereira <helder.pereira@xing.com>
2019-03-25 21:12:21 +0100

    added palindrome

--- lesson 5/p7-strings.py
+++ lesson 5/p7-strings.py
@@ -1,13 +1,29 @@
-name = "James"
+name = "bom mob"
 
 #homework:
 # ask the user for a string and tell the user if it is a palindrome or not
 
 
 #small hint (maybe)
-for i in range(0, 10):
-    print(i) #print the numbers
-    print(9-i) #print the numbers in reverse
-    print("-----")
+# for i in range(0, 10):
+#     print(i) #print the numbers
+#     print(9-i) #print the numbers in reverse
+#     print("-----")
+
+def isPalindrome(word):
+
+    for i in range(0, int(len(word) / 2)):
+        mirrorPos = len(word)-1-i
+
+        letter = word[i]
+        mirrorLetter = word[mirrorPos]
+
+        if letter != mirrorLetter:
+            return False
+
+    return True
+
+print(isPalindrome("hanh"))
+
 
 
